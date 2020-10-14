ffe748161d3196bc5cbb2db814a55e5afab0e151
Ivan Kolesnev <i.v.kolesnev@mail.ru>
2020-10-14 23:01:50 +0300

    feat: add reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    let numberMinus = n < 0 ? n * -1 : n;
+    let numberWithNull = n % 10 === 0 ? n / 10 : n;
+    let numberString = `${numberMinus}` || `${numberWithNull}`;
+
+    return Number(numberString.split("").reverse().join(""));
+};
